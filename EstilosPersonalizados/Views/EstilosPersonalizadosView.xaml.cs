16777250c6936269b6b367a504d32057097813ca
Moperz96 <37333306+Moperz96@users.noreply.github.com>
2018-06-05 20:39:04 +0200

    Initial commit

---
+++ EstilosPersonalizados/Views/EstilosPersonalizadosView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EstilosPersonalizados.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EstilosPersonalizadosView : ContentPage
+	{
+		public EstilosPersonalizadosView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
